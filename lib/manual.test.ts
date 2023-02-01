9f9bc739cf7a5a50772ee7848c15a766d9608b4a
Paul Beynon <paul@beynon.app>
2023-02-01 12:50:44 +1100

    TEST: Override manually mocked values

--- lib/manual.test.ts
+++ lib/manual.test.ts
@@ -1,10 +1,10 @@
-import * as manual from './manual';
+import * as dependency from './manual';
 
 jest.mock('./manual');
+const manual = jest.mocked(dependency);
 
-
-describe('lib.manual', () => {
-  it('should get the manually mocked value for manual.get', () => {
+describe('lib.manual.get', () => {
+  it('should provide the manually mocked value', () => {
     expect.assertions(1);
 
     const result = manual.get('a b c');
@@ -12,12 +12,13 @@ describe('lib.manual', () => {
     expect(result).toStrictEqual(['a', 'b', 'c']);
   });
 
-  it('should put the manually mocked value for manual.put', () => {
+  it('should override the manually mocked value', () => {
     expect.assertions(1);
 
-    const result = manual.put(['a', 'b', 'c']);
+    manual.get.mockReturnValueOnce(['d', 'e', 'f']);
+    const result = manual.get('a b c');
 
-    expect(result).toBe('a b c');
+    expect(result).toStrictEqual(['d', 'e', 'f']);
   });
 
   it('should provide a value to manual.get', () => {
@@ -26,6 +27,25 @@ describe('lib.manual', () => {
     manual.get('a b c');
 
     expect(manual.get).toHaveBeenCalledWith('a b c');
+  });
+});
+
+describe('lib.manual.put', () => {
+  it('should provide the manually mocked value', () => {
+    expect.assertions(1);
+
+    const result = manual.put(['a', 'b', 'c']);
+
+    expect(result).toBe('a b c');
+  });
+
+  it('should override the manually mocked value', () => {
+    expect.assertions(1);
+
+    manual.put.mockReturnValueOnce('d e f');
+    const result = manual.put(['a', 'b', 'c']);
+
+    expect(result).toBe('d e f');
   });
 
   it('should provide a value to manual.put', () => {
